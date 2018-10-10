0acb131b16613f0a64492e0fb8804bec8dc25227
Song Zhou <zhousong1993@gmail.com>
2018-10-10 13:01:10 +0800

    initial commit

---
+++ SZAutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SZAutoLayout
+//
+//  Created by songzhou on 2018/10/10.
+//  Copyright © 2018 songzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SZAutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SZAutoLayout
+//
+//  Created by songzhou on 2018/10/10.
+//  Copyright © 2018 songzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
